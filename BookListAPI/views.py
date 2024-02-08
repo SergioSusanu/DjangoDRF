c2208312a7c76c1cbe16ad65ade624e2745bf248
SergioSusanu <Sergio.susanu.developer@gmail.com>
2024-02-08 18:51:14 +0200

    class based DRF get post put requests

--- BookListAPI/views.py
+++ BookListAPI/views.py
@@ -3,8 +3,32 @@ from django.shortcuts import render
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.decorators import api_view
+from rest_framework.views import APIView
 
-@api_view(['POST','GET'])
-def books(request):
-    return Response('list of the books',
-                    status=status.HTTP_200_OK)
+# @api_view(['POST','GET'])
+# def books(request):
+#     return Response('list of the books',
+#                     status=status.HTTP_200_OK)
+
+class BookList(APIView):
+    def get(self, request):
+        author = request.GET.get('author')
+        if (author):
+            return Response({'author':author},status.HTTP_200_OK)
+        return Response({'msg':'from class get'},
+                        status.HTTP_200_OK)
+
+    def post(self,request):
+        bookTitle = request.data.get('title')
+        if (bookTitle):
+            return Response({'title':bookTitle},status.HTTP_201_CREATED)
+        return Response({'msg':'class post method'},
+                        status.HTTP_201_CREATED)
+
+class Book(APIView):
+    def get(self, request, pk):
+        return Response({'book id':str(pk)}, status.HTTP_200_OK)
+
+    def put(self, request, pk):
+        return Response({'id and title':str(pk) + request.data.get('title'),
+                         },status.HTTP_200_OK)
